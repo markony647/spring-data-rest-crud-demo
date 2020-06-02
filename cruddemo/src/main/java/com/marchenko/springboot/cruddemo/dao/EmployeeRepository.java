cfa9e7b05fef0c134b8deda9066721c7fe6bf44f
Vitalii Marchenko <vitalymarchenko@Vitalys-MacBook-Pro.local>
2020-06-02 09:49:12 +0300

    Add demo project for Spring Data REST

---
+++ cruddemo/src/main/java/com/marchenko/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.marchenko.springboot.cruddemo.dao;
+
+import com.marchenko.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
---
+++ cruddemo/src/main/java/com/marchenko/springboot/cruddemo/entity/Employee.java
@@ -0,0 +1,27 @@
+package com.marchenko.springboot.cruddemo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+@Data
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+}
